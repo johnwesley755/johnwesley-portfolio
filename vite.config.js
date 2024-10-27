c2f7b66c89b724731f8b39f513ee572da285705a
John Wesley L <148236413+johnwesley755@users.noreply.github.com>
2024-10-27 22:50:58 +0530

    Updated committed message

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
+  base: "/johnwesley-portfolio",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            // Group node_modules into separate chunks
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 500, // Set your desired limit (in kB)
-  },
 });
